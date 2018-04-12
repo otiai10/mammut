90d9dc4656d0c7d532c834fd3003a3db98de0947
Hiromu OCHIAI <otiai10@gmail.com>
2018-04-12 14:51:14 +0900

    Make the redirect_uris for client optional

--- src/Instance/index.ts
+++ src/Instance/index.ts
@@ -3,6 +3,7 @@ import Client from "../Client";
 export declare interface IClientOption {
     name: string;
     scopes: string; // Space separated scopes
+    redirect?: string;
 }
 
 /**
@@ -21,11 +22,14 @@ export default class Instance {
     }
 
     public client(opt: IClientOption): Promise<Client> {
+
+        opt.redirect = opt.redirect || "urn:ietf:wg:oauth:2.0:oob";
+
         const url = new URL(this.url.href);
         url.pathname = "/api/v1/apps";
         const formdata = new FormData();
         formdata.append("client_name", opt.name);
-        formdata.append("redirect_uris", "urn:ietf:wg:oauth:2.0:oob");
+        formdata.append("redirect_uris", opt.redirect);
         formdata.append("scopes", opt.scopes);
         return fetch(url.href, {
             body: formdata,
