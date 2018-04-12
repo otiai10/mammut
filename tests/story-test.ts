dfe12dde89b4cd3144dd38f6b1ca0da6d012d378
Hiromu OCHIAI <otiai10@gmail.com>
2018-04-12 14:35:56 +0900

    Add skips for story tests

--- tests/story-test.ts
+++ tests/story-test.ts
@@ -1,3 +1,8 @@
 /**
  * TODO: Here tests for user stories should be.
  */
+describe.skip("User Story 1", () => {
+    test.skip("do something", () => {
+        expect("something").toBe("something");
+    });
+});
